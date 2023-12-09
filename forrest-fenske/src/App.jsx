1ba0d8bbcf53f40986bcc9a7cb4f84fee72f1343
gmolk <galen.molk@onepeloton.com>
2023-12-09 10:00:21 -0500

    updating logo and header

--- forrest-fenske/src/App.jsx
+++ forrest-fenske/src/App.jsx
@@ -4,34 +4,36 @@ import viteLogo from '/vite.svg'
 import logo from '../public/logo.png'
 import portrait from '../public/portrait.jpg'
 import './App.css'
+import header from './assets/header.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='main-header'>WORM TOMB</h1>
-      <div className='card'>
-        <a href="https://fineartamerica.com/profiles/forrest-fenske" target='_blank'>
-          <img src={logo} className='logo' alt='Logo' />
-        </a>
-      </div>
-      <h2 className='main-header'>Recycled Art & Tool</h2>
-      <img src={portrait} className='portrait'></img>
-      {/*
-      <button>hello</button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-       */}
+        <img src={header} alt='header' className='header-image'/>
+        <h1 className='main-header'>WORM TOMB</h1>
+        <div className='card'>
+            <a href="https://fineartamerica.com/profiles/forrest-fenske" target='_blank'>
+                <img src={logo} className='logo' alt='Logo' />
+            </a>
+        </div>
+        <h2 className='main-header'>Recycled Art & Tool</h2>
+        <img src={portrait} className='portrait' alt='portrait'></img>
+        {/*
+          <button>hello</button>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.jsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+           */}
     </>
   )
 }
