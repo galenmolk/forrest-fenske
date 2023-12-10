36505ad6e3ff7e513596948e1d4ae21bd968e005
gmolk <galen.molk@onepeloton.com>
2023-12-10 12:49:59 -0500

    responsiveness

--- forrest-fenske/src/App.jsx
+++ forrest-fenske/src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import header from './assets/img/header.png'
 import portrait from './assets/img/forrest-dragon-portrait.png'
 import Spacer from "./Spacer.jsx";
+import siteData from './assets/site-data.json'
 
 function App() {
   return (
       <>
           <img src={header} alt='Womb Tomb – Recycled Art & Tool' className='header'/>
           <div className='about-artist'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              <img src={portrait} className='portrait' alt='Forrest Fenske, Artist. Metal Dragon Sculpture.'></img>
-              </p>
+              <span>
+                <p>{siteData.bio}</p>
+              </span>
+              <span>
+                <img src={portrait} alt='Forrest Fenske, Artist. Metal Dragon Sculpture.'></img>
+            </span>
           </div>
           <Spacer space={'2rem'}/>
       </>
