0148c195071f48a0b71afc5a7af77d432723a6db
gmolk <galen.molk@onepeloton.com>
2023-12-10 11:41:11 -0500

    GH Pages

--- forrest-fenske/vite.config.js
+++ forrest-fenske/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/galenmolk/forrest-fenske.git',
   plugins: [react()],
 })
