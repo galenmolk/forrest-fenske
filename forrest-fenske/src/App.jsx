d983ed73c4d41dc621bbb7389e2c6075800fd5e5
gmolk <galen.molk@onepeloton.com>
2023-12-16 19:07:55 -0500

    bio

--- forrest-fenske/src/App.jsx
+++ forrest-fenske/src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import header from './assets/img/header.webp'
 import portrait from './assets/img/forrest-dragon-portrait.webp'
 import Spacer from "./Spacer.jsx";
-import siteData from './assets/site-data.json'
 import PageCard from './PageCard.jsx'
 import drawingThumbnail from './assets/img/drawing-thumbnail.webp'
 import sculptureThumbnail from './assets/img/sculpture-thumbnail.webp'
@@ -10,16 +9,18 @@ import filmThumbnail from './assets/img/film-thumbnail.webp'
 import musicThumbnail from './assets/img/music-thumbnail.webp'
 
 function App() {
+    const email = 'wormtombcontact@gmail.com';
+    
     return (
         <>
             <img src={header} alt='Womb Tomb – Recycled Art & Tool' className='header'/>
             <div className='about-artist'>
-                <span>
-                    <p>{siteData.bio}</p>
-                </span>
-                <span>
-                    <img src={portrait} alt='Forrest Fenske, Artist. Metal Dragon Sculpture.'></img>
-                </span>
+                <div>
+                    <p>From under rotten logs and oozing out of dumpsters, Forrest Fenske weaves materials and inspirations from the world around him into interdisciplinary symphonies of experience. Venture deeper to bear witness.</p>
+                    <p>Shop links below.</p>
+                    <p>E-mail <a href={`mailto:${email}`} target='_blank'>wormtombcontact@gmail.com</a> for commission inquiries or any other questions.</p>
+                </div>
+                <img src={portrait} alt='Forrest Fenske, Artist. Metal Dragon Sculpture.'></img>
             </div>
             <div className='pages'>
                 <PageCard title='Sculpture' img={sculptureThumbnail} width='114%' url='https://www.instagram.com/worm_tomb/'/>
