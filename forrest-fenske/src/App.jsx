e639bfd77d93bdf5e1cd52f8d19a4a5b6bff9ce6
gmolk <galen.molk@onepeloton.com>
2023-12-23 17:11:02 -0500

    responsiveness improvements

--- forrest-fenske/src/App.jsx
+++ forrest-fenske/src/App.jsx
@@ -1,34 +1,40 @@
 import './App.css'
 import header from './assets/img/header.webp'
 import portrait from './assets/img/forrest-dragon-portrait.webp'
-import Spacer from "./Spacer.jsx";
 import PageCard from './PageCard.jsx'
 import drawingThumbnail from './assets/img/drawing-thumbnail.webp'
 import sculptureThumbnail from './assets/img/sculpture-thumbnail.webp'
-import filmThumbnail from './assets/img/film-thumbnail.webp'
 import musicThumbnail from './assets/img/music-thumbnail.webp'
+import border from './assets/img/border.png'
+import ocd from './assets/img/ocd.jpg'
 
 function App() {
     const email = 'wormtombcontact@gmail.com';
     
     return (
         <>
-            <img src={header} alt='Womb Tomb – Recycled Art & Tool' className='header'/>
+            <div>
+                <img src={header} alt='Womb Tomb – Recycled Art & Tool' className='header'/>
+                <img src={border} alt='Website border' className='border'/>
+            </div>
             <div className='about-artist'>
-                <div>
-                    <p>From under rotten logs and oozing out of dumpsters, Forrest Fenske weaves materials and inspirations from the world around him into interdisciplinary symphonies of experience. Venture deeper to bear witness.</p>
-                    <p>Shop links below.</p>
-                    <p>E-mail <a href={`mailto:${email}`} target='_blank'>wormtombcontact@gmail.com</a> for commission inquiries or any other questions.</p>
-                </div>
                 <img src={portrait} alt='Forrest Fenske, Artist. Metal Dragon Sculpture.'></img>
+                <div>
+                    <p>From under rotten logs and oozing out of dumpsters, Forrest Fenske weaves materials and
+                        inspirations from the world around him into interdisciplinary symphonies of experience.
+                        Venture deeper to bear witness.</p>
+                    <p>Shop links below.</p>
+                    <p>For commission inquiries or any other questions, email <a href={`mailto:${email}`} target='_blank'>wormtombcontact@gmail.com</a>.</p>
+                </div>
             </div>
             <div className='pages'>
-                <PageCard title='Sculpture' img={sculptureThumbnail} width='114%' url='https://www.instagram.com/worm_tomb/'/>
-                <PageCard title='Drawing' img={drawingThumbnail} url='https://www.patreon.com/WormTomb'/>
-                <PageCard title='Film' img={filmThumbnail} width={'137%'} url='https://www.youtube.com/@wormtomb4168'/>
-                <PageCard title='Music' img={musicThumbnail} width={'136%'} url='https://wormtomb.bandcamp.com/'/>
-            </div>  
-            <Spacer space={'4rem'}/>
+                <PageCard title='Sculpture' img={sculptureThumbnail} width='300%'
+                              url='https://www.instagram.com/worm_tomb/' />
+                    <PageCard title='Drawing' img={drawingThumbnail} url='https://www.patreon.com/WormTomb' width='120%'/>
+                    <PageCard title='Film' img={ocd} width={'179%'}
+                              url='https://www.youtube.com/@wormtomb4168'/>
+                    <PageCard title='Music' img={musicThumbnail} width={'179%'} url='https://wormtomb.bandcamp.com/'/>
+            </div>
         </>
     )
 }
--- forrest-fenske/src/PageCard.jsx
+++ forrest-fenske/src/PageCard.jsx
@@ -1,18 +1,18 @@
 import buttonBanner from './assets/img/button-banner.webp'
 
-export default function PageCard({title, img, width, url}) {
+export default function PageCard({title, img, width, url, margins}) {
     return (
         <a href={url} target='_blank'>
-        <div className='page-card'>
-            <img src={buttonBanner} alt='Button Background Banner'/>
-            <p>{title}</p>
-            <div className='page-card-thumbnail-mask'>
-                <img className='page-card-thumbnail' 
-                     src={img} 
-                     alt={`${title} thumbnail`}
-                     style={{ width: width ?? '100%' }}/>
+            <div className='page-card'>
+                <div className='page-card-thumbnail-mask'>
+                    <img className='page-card-thumbnail'
+                         src={img}
+                         alt={`${title} thumbnail`}
+                         style={{width: width ?? '100%', margins: margins }}/>
+                </div>
+                <img className='page-banner' src={buttonBanner} alt='Button Background Banner'/>
+                <p>{title}</p>
             </div>
-        </div>
         </a>
     );
 }
