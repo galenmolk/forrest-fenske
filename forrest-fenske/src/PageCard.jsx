76fea236ac93995b72e364a0ea965577b8d30271
gmolk <galen.molk@onepeloton.com>
2023-12-24 08:53:41 -0500

    tweaks and image opt

--- forrest-fenske/src/PageCard.jsx
+++ forrest-fenske/src/PageCard.jsx
@@ -1,6 +1,6 @@
 import buttonBanner from './assets/img/button-banner.webp'
 
-export default function PageCard({title, img, width, url, margins}) {
+export default function PageCard({title, img, width, url, margin, top, left}) {
     return (
         <a href={url} target='_blank'>
             <div className='page-card'>
@@ -8,7 +8,11 @@ export default function PageCard({title, img, width, url, margins}) {
                     <img className='page-card-thumbnail'
                          src={img}
                          alt={`${title} thumbnail`}
-                         style={{width: width ?? '100%', margins: margins }}/>
+                         style={{width: width ?? '100%', 
+                             margin: margin || '0',
+                             top: top || '0%',
+                             left: left || '0%'
+                         }}/>
                 </div>
                 <img className='page-banner' src={buttonBanner} alt='Button Background Banner'/>
                 <p>{title}</p>
