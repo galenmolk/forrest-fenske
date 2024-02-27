18073add59f31249a588d59cf2e0bb941d88787c
gmolk <galenmolk@gmail.com>
2024-02-26 21:07:17 -0500

    clean up

--- forrest-fenske/src/pages/Lanterns.jsx
+++ forrest-fenske/src/pages/Lanterns.jsx
@@ -6,31 +6,14 @@ import Footer from "../Footer.jsx";
 import PageDescription from "../PageDescription/PageDescription.jsx";
 import {dummy} from "../Dummy.js";
 import {useEffect, useState} from "react";
-import axios from "axios";
 import {getImages} from "../cloudinary/Cloudinary.js";
 
 export default function Lanterns() {
     const [lanterns, setLanterns] = useState([]);
     
     useEffect(() => {
-        getImages("https://res.cloudinary.com/cloudkeeper/image/list/lantern.json", setLanterns)
-        /*const fetchLanterns = async () => {
-            try {
-                const response = await axios.get("https://res.cloudinary.com/cloudkeeper/image/list/lantern.json")
-                setLanterns(response.data.resources.map(r => {
-                    const url = `https://res.cloudinary.com/cloudkeeper/image/upload/v${r.version}/${r.public_id}.jpg`;
-                    return {
-                        original: url,
-                        thumbnail: url,
-                        thumbnailClass: 'thumbnail'
-                    }
-                }))
-            } catch (error) {
-                console.error(error)
-            }
-        };
-        
-        fetchLanterns();*/
+        getImages("https://res.cloudinary.com/cloudkeeper/image/list/lantern.json", 
+            setLanterns)
     }, []);
     
   return (
